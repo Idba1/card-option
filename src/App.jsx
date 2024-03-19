7b258d6b884c5005f31f3d645f4a82b092116ccb
Idba1 <moniraislamself@gmail.com>
2024-03-19 17:04:01 +0600

    add Daisy Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import DaisyNav from './Components/Daisy/DaisyNav'
 
 function App() {
 
   return (
     <>
+      <DaisyNav></DaisyNav>
       <h1 className='bg-red-900 text-4xl text-slate-50'>Vite + React</h1>
+      
     </>
   )
 }
