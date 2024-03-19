6e0910d1b58d4db020e939a8b6e8fadbada8873b
Idba1 <moniraislamself@gmail.com>
2024-03-19 17:37:28 +0600

    responsive for medium device

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -10,9 +10,9 @@ const Navbar = () => {
       
     return (
         <div>
-            <ul>
+            <ul className="md:flex">
             {
-                routes.map(route =>  <li key={route.id}><a href={route.path}>{route.name}</a></li>)
+                routes.map(route =>  <li className="mr-10" key={route.id}><a href={route.path}>{route.name}</a></li>)
             }
             </ul>
         </div>
