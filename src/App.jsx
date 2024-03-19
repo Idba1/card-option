af594dded1ebf5c60b9d6431e5a830be67483273
Idba1 <moniraislamself@gmail.com>
2024-03-19 17:34:23 +0600

    create data for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import DaisyNav from './Components/Daisy/DaisyNav'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
+      <Navbar></Navbar>
       <DaisyNav></DaisyNav>
       <h1 className='bg-red-900 text-4xl text-slate-50'>Vite + React</h1>
       
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,20 @@
 const Navbar = () => {
+
+    const routes =[
+        { path: "/", name: "Home", id: 1 },
+        { path: "/about", name: "About", id: 2 },
+        { path: "/services", name: "Services", id: 3 },
+        { path: "/contact", name: "Contact", id: 4 },
+        { path: "/blog", name: "Blog", id: 5 }
+      ]
+      
     return (
         <div>
-            
+            <ul>
+            {
+                routes.map(route =>  <li key={route.id}><a href={route.path}>{route.name}</a></li>)
+            }
+            </ul>
         </div>
     );
 };
