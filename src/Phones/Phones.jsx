9ebb571152d56ff9846998d277c97711d78589af
Idba1 <moniraislamself@gmail.com>
2024-03-20 04:48:38 +0600

    add loading

--- src/Phones/Phones.jsx
+++ src/Phones/Phones.jsx
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { BarChart, Bar, XAxis, YAxis, Tooltip} from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
+import { Audio } from 'react-loader-spinner';
 
 
 const Phones = () => {
 
     const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
     useEffect(() => {
         axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
             .then(data => {
@@ -13,25 +15,35 @@ const Phones = () => {
                 const phoneWithFackData = phoneData.map(Phone => {
                     const obj = {
                         name: Phone.phone_name,
-                        price: parseInt(Phone.slug.split('-') [1])
+                        price: parseInt(Phone.slug.split('-')[1])
                     }
                     return obj;
                 })
                 console.log(phoneWithFackData);
-                setData(phoneWithFackData)
+                setData(phoneWithFackData);
+                setLoading(false)
             })
     }, [])
     return (
         <div>
+            {loading && <Audio
+                height="80"
+                width="80"
+                radius="9"
+                color="green"
+                ariaLabel="three-dots-loading"
+                wrapperStyle
+                wrapperClass
+            />}
             <h1 className="text-2xl text-blue-500">Total Phones : {data.length}</h1>
             {/* <ResponsiveContainer width="100%" height="100%"> */}
-                <BarChart width={1200} height={600} data={data}>
-                    {/* <Bar dataKey="name" fill="#8884d8" /> */}
-                    <Bar dataKey={"price"} fill="#87cefa" />
-                    <XAxis dataKey={name}></XAxis>
-                    <YAxis></YAxis>
-                    <Tooltip></Tooltip>
-                </BarChart>
+            <BarChart width={1200} height={600} data={data}>
+                {/* <Bar dataKey="name" fill="#8884d8" /> */}
+                <Bar dataKey={"price"} fill="#87cefa" />
+                <XAxis dataKey={name}></XAxis>
+                <YAxis></YAxis>
+                <Tooltip></Tooltip>
+            </BarChart>
             {/* </ResponsiveContainer> */}
         </div>
     );
